84e6a5f7c5863f9f526760b9fe8aaf3b0f1186d8
jonames <gyjhkx57164@027168.com>
2017-08-04 11:30:05 +0000

    1st commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Vue-Appstore
+纯粹学习。
+
+## 用到的技术
+vue2 + vue-router + vue-resource + webpack + Vux + Less + ES6
+
+
+## Build Setup
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
---
+++ src/components/common/tabnav.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <tabbar>
+      <tabbar-item selected link="/">
+        <img slot="icon" src="static/images/feature.png">
+        <img slot="icon-active" src="static/images/feature_focus.png">
+        <span slot="label">精品推荐</span>
+      </tabbar-item>
+      <tabbar-item link="/top">
+        <img slot="icon" src="static/images/top.png">
+        <img slot="icon-active" src="static/images/top_focus.png">
+        <span slot="label">排行榜</span>
+      </tabbar-item>
+      <tabbar-item show-dot link="/game">
+        <img slot="icon" src="static/images/game.png">
+        <img slot="icon-active" src="static/images/game_focus.png">
+        <span slot="label">游戏</span>
+      </tabbar-item>
+      <tabbar-item badge="3" link="/soft">
+        <img slot="icon" src="static/images/soft.png">
+        <img slot="icon-active" src="static/images/soft_focus.png">
+        <span slot="label">软件</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import { Tabbar, TabbarItem, Group, Cell } from 'vux'
+export default {
+  components: {
+    Tabbar,
+    TabbarItem,
+    Group,
+    Cell
+  }
+}
+</script>
+<style scoped>
+  .weui-tabbar{position: fixed;}
+</style>
